3f964a4b74c0052b232fd938c1fc0c23debdcb19
izemskov <pascal.ilya@gmail.com>
2025-03-14 15:30:02 +0100

    Fix collation rule

--- Java/StringCollation/src/main/java/ru/develgame/codelab/java/stringcollation/Main.java
+++ Java/StringCollation/src/main/java/ru/develgame/codelab/java/stringcollation/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        String ruRule = "< 0 < 1 < 2 < 3 < 4 < 5 < 6 < 7 < 8 < 9 " +
+        String ruRule = "< ' ' < 0 < 1 < 2 < 3 < 4 < 5 < 6 < 7 < 8 < 9 " +
                 "< a < b < c < d < e < f < g < h < i < j < k < l < m < n < o < p < q < r < s < t < u < v < w < x < y < z " +
                 "< а < б < в < г < д < е < ё < ж < з < и < й < к < л < м < н < о < п < р < с < т < у < ф < х < ц < ч < ш < щ < ъ < ы < ь < я < ю < я";
         RuleBasedCollator collator = new RuleBasedCollator(ruRule);
