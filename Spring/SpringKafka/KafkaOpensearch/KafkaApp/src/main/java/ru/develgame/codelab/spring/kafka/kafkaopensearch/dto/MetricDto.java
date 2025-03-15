2c47d329f7d7aee3bb30ff14b2c5a2d9325cf144
izemskov <pascal.ilya@gmail.com>
2025-03-15 01:25:51 +0100

    Kafka connect

--- Spring/SpringKafka/KafkaOpensearch/KafkaApp/src/main/java/ru/develgame/codelab/spring/kafka/kafkaopensearch/dto/MetricDto.java
+++
@@ -1,6 +0,0 @@
-package ru.develgame.codelab.spring.kafka.kafkaopensearch.dto;
-
-import java.util.Date;
-
-public record MetricDto(Date currentDate, int cpuUsage) {
-}
--- Spring/SpringKafka/KafkaOpensearch/KafkaApp/src/main/java/ru/develgame/codelab/spring/kafka/kafkaopensearch/workers/MessageScheduler.java
+++ Spring/SpringKafka/KafkaOpensearch/KafkaApp/src/main/java/ru/develgame/codelab/spring/kafka/kafkaopensearch/workers/MessageScheduler.java
@@ -5,24 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import ru.develgame.codelab.spring.kafka.kafkaopensearch.dto.MetricDto;
 
 import java.lang.management.ManagementFactory;
-import java.util.Date;
 
 @Component
 public class MessageScheduler {
     @Autowired
-    private KafkaTemplate<String, MetricDto> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     public static final String TOPIC_NAME_METRICS = "metricsTopicOpensearch";
 
     @Scheduled(fixedDelay = 1000L)
     public void sendMessage() {
         OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-        MetricDto metricDto = new MetricDto(new Date(), (int) (osBean.getCpuLoad() * 100));
 
-        kafkaTemplate.send(TOPIC_NAME_METRICS, metricDto).whenComplete((stringMetricDtoSendResult, throwable) ->
-                System.out.println(stringMetricDtoSendResult.getRecordMetadata().offset()));
+        kafkaTemplate.send(TOPIC_NAME_METRICS,
+                        """
+                        {
+                            "cpu": %d
+                        }
+                        """.formatted((int) (osBean.getCpuLoad() * 100)))
+                .whenComplete((stringMetricDtoSendResult, throwable) ->
+                        System.out.println(stringMetricDtoSendResult.getRecordMetadata().offset()));
     }
 }
